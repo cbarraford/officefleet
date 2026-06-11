47ab31975a6cdc36af41cb27c7b2295fa7b0e4a9
Son of Odin <2700975-son-of-odin@users.noreply.gitlab.com>
2026-06-11 17:44:05 -0400

    fix(dogfood): migration 002 guard also catches duplicate_table (fresh-install migrate was broken)

--- internal/db/migrations/002_unique_constraints.sql
+++ internal/db/migrations/002_unique_constraints.sql
@@ -1,17 +1,17 @@
 -- +migrate Up
 DO $$ BEGIN
     ALTER TABLE agents ADD CONSTRAINT agents_name_unique UNIQUE (name);
-EXCEPTION WHEN duplicate_object THEN NULL;
+EXCEPTION WHEN duplicate_object OR duplicate_table THEN NULL;
 END $$;
 
 DO $$ BEGIN
     ALTER TABLE duties ADD CONSTRAINT duties_name_unique UNIQUE (name);
-EXCEPTION WHEN duplicate_object THEN NULL;
+EXCEPTION WHEN duplicate_object OR duplicate_table THEN NULL;
 END $$;
 
 DO $$ BEGIN
     ALTER TABLE assignments ADD CONSTRAINT assignments_agent_duty_unique UNIQUE (agent_id, duty_id);
-EXCEPTION WHEN duplicate_object THEN NULL;
+EXCEPTION WHEN duplicate_object OR duplicate_table THEN NULL;
 END $$;
 
 -- +migrate Down
